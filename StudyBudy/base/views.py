18a1ec30ea4c40fe791944f5d2d92c26a737fab1
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-06-19 17:42:20 -0300

    inicializando, configurações iniciais

---
+++ StudyBudy/base/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+rooms = [
+    {'id': 1, 'name': 'Lets learn python!'},
+    {'id': 2, 'name': 'Design with me!'},
+    {'id': 3, 'name': 'Frontend developers!'},
+]
