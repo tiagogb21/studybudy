b71d6e13930b24b773bfd9c03665a1a24cab4ae2
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-06-19 18:32:44 -0300

    criando as rotas para html e room

--- StudyBudy/base/views.py
+++ StudyBudy/base/views.py
@@ -5,3 +5,17 @@ rooms = [
     {'id': 2, 'name': 'Design with me!'},
     {'id': 3, 'name': 'Frontend developers!'},
 ]
+
+
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
