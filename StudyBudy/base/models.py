85f3f1aa23414443ca9ce509fba20e915c7d582a
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-06-20 09:37:19 -0300

    criando a models message

--- StudyBudy/base/models.py
+++ StudyBudy/base/models.py
@@ -12,3 +12,17 @@ class Room(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Message(models.Model):
+    # user =
+    # ForeignKey --> permite a conexão com outra classe
+    # CASCADE --> quando deletado, deleta todos os descendentes
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    update = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        # retorna os 50 primeiros caracteres
+        return self.body[0:50]
--- StudyBudy/base/views.py
+++ StudyBudy/base/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from .models import Room
 
-rooms = [
-    {'id': 1, 'name': 'Lets learn python!'},
-    {'id': 2, 'name': 'Design with me!'},
-    {'id': 3, 'name': 'Frontend developers!'},
-]
+# rooms = [
+#     {'id': 1, 'name': 'Lets learn python!'},
+#     {'id': 2, 'name': 'Design with me!'},
+#     {'id': 3, 'name': 'Frontend developers!'},
+# ]
 
 
 def home(request):
@@ -15,9 +15,6 @@ def home(request):
 
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
